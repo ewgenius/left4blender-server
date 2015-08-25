43e2cb285f92debf1d743df9b8b7fc84cfa15611
ewgenius <ewgeniux@gmail.com>
2015-08-25 16:55:48 +0300

    chat

--- server.js
+++ server.js
@@ -2,20 +2,26 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendfile(__dirname + '/public/index.html');
 });
 
-var rooms = {};
-
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     console.log('connected');
 
-    socket.on('disconnect', function(){
+    socket.on('system.message', function (message) {
+        console.log('system:', message);
+    });
+
+    socket.on('chat.message', function (message) {
+        console.log('chat:', message);
+    });
+
+    socket.on('disconnect', function () {
         console.log('disconnected');
     });
 });
 
-server.listen(3000, function() {
+server.listen(3000, function () {
 
 });
